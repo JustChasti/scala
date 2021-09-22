9aa8d19c48b5c147613017cceffd17ed1eebb5db
Chasti <chastytim@mail.ru>
2021-09-22 10:59:59 +0300

    lr1 complite

--- lr1/lr1.scala
+++ lr1/lr1.scala
@@ -83,11 +83,18 @@ def Task13(){
 
 
 def Task14(){
-    println(Compose("Hello"))
+    println(T14C("Hello")) 
 }
 
 
-// Task 16 основан на 15, 15 - нет
+def T14C(s2: String): Long = { 
+    s2.foldLeft(1L)((m, n) => m * n) 
+}
+
+
+def Task16(){
+    println(Compose("Hello"))
+}
 
 
 def Compose(text: String): Long = {
@@ -109,7 +116,6 @@ def Task17(n: Double): Double = {
         x = 1
     else
         x = n
-}*/
 
 
 def Task19(){
@@ -143,7 +149,234 @@ def Opener(l: List[Any]): Unit = {
         println(newS.toList.flatten)
     }
     result
+}*/
+
+
+def valid(toString: String): Boolean = {
+    var a = ListBuffer[Int]()
+    for (w <- 0 until toString.length) {
+      if (!a.contains(toString(w))) {
+        a += toString(w)
+      }
+      else {
+        return false
+      }
+    }
+    true
 }
 
 
-Task19()
+
+def Task18(m: Int, n: Int){
+    var a: Int = 0
+    for (w <- m to n; if (valid(w.toString))) {
+        a += w;
+    }
+    println(a)
+}
+
+
+def Task19(m: List[Any]){
+    var s = 1::Nil
+    s = s.filter(_<2)
+    val s2 = m.toString();
+    for(v<-0 until s2.length){
+      if(s2(v).toInt>47&&s2(v).toInt<58){
+        s = s2(v).toString.toInt::s
+      }
+    }
+    println(s.reverse)
+}
+
+
+
+def Task20(n: Int){
+    var a:Int = 1
+    for (i <- 2 until n) {
+      if (n % i == 0) {
+        a=i;
+      }
+    }
+    var c = 0
+    for (w <- 0 until a.toString.length) {
+      c+=a.toString.charAt(w).toString.toInt
+    }
+    println(c)
+}
+
+
+def Task21(k:Int, m: List[Any]){
+    var c = List[Any]()
+    for(i<- 0 until m.length){
+      for(j<- 0 until k){
+        c = m(i)::c
+      }
+    }
+    println(c)
+}
+
+
+def Task22(n: Int){
+    var a:Int = 1
+    for (i <- 2 until n) {
+      if (n % i == 0) {
+        a=i;
+      }
+    }
+    var c = 0
+    for (w <- 0 until a.toString.length) {
+      c+=a.toString.charAt(w).toString.toInt
+    }
+    println(c)
+}
+
+
+def Task23(k:Int, m: List[Any]){
+    var c = List[Any]()
+    for(i<- 0 until m.length){
+      for(j<- 0 until k){
+        c = m(i)::c
+      }
+    }
+    println(c)
+}
+
+
+def gcd(a: Int, b: Int): Int = {
+    if(b == 0){
+      a
+    }
+    else {
+      gcd(b,a % b);
+    }
+}
+
+
+def Task24(n: Int, m: Int){
+    println(n / gcd(n, m) * m)
+}
+
+
+def Task25(k:Int, m: List[Any]){
+    var c = List[Any]()
+    for(i<- 0 until m.length){
+      if((i+1)%k != 0){
+        c = m(i)::c
+      }
+    }
+    println(c.reverse)
+}
+
+
+def faktorial(n:Int):Int={
+    if(n<2){
+      1
+    }
+    else {
+      n*faktorial(n-1)
+    }
+}
+
+
+def Task26(n: Int, k: Int){
+    println(faktorial(n)/faktorial(n-k))
+}
+
+
+def Task27(k:Int, m: List[Any]) : List[Any]={
+    var c = List[Any]()
+    var i = k
+    if(i>0){
+      for(i<- 1 until m.length){
+        c = m(i)::c
+      }
+      c = c.reverse
+      val b = m(0)
+      c = c:+b
+      i -= 1
+    }
+    else {
+      for(i<- 0 until m.length-1){
+        c = m(i)::c
+      }
+      c = c.reverse
+      val a = m(m.length-1)
+      c = a::c
+      i += 1
+    }
+    if(i!=0){
+      return Task27(i, c)
+    }
+    c
+}
+
+
+def summdel(n: Int): Int = {
+    var c = 1
+    for(v <- 2 until n){
+      if(n%v==0)
+        c+=v
+    }
+    c
+}
+
+
+def Task28(n: Int): Int ={
+    for(v <- 0 to n-1){
+      if(n-v==summdel(n-v))
+        return n-v
+    }
+    0
+}
+
+
+def Task29(m: List[Any]){
+    var c1 = List[Any]()
+    var c2 = List[Any]()
+    for(i<- 0 until m.length){
+      if(i%2 == 0){
+        c1 = m(i)::c1
+      }
+      else {
+        c2 = m(i)::c2
+      }
+    }
+    println(c1.reverse)
+    println(c2.reverse)
+}
+
+
+def Task30(n: Int): Int = {
+    var count = 1
+    var sum = 0
+    for(i<-n to 1 by -1){
+      var a = i
+      while (a>0){
+        sum+=a%10
+        a/=10
+      }
+      if(sum>1){
+        val b = sum
+        while (sum<i){
+          sum*= b
+          count+=1
+        }
+      }
+      if(sum == i && count !=1 )return sum
+      sum=0
+    }
+    0
+}
+
+
+def Task31(m: List[(Int, String)]){
+    var c1 = List[Int]()
+    var c2 = List[String]()
+    for(i<- 0 until m.length){
+      c1 = m(i)._1::c1
+      c2 = m(i)._2::c2
+    }
+    println(c1.reverse)
+    println(c2.reverse)
+}
+
--- lr1/test.scala
+++
@@ -1,2 +0,0 @@
-var y = "test"
-Console.printf(y)
